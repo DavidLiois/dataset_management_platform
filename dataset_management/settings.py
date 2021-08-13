3e9f418373833c23b4e809360f9d3efdce2d6468
dap0d <thenastytanker@gmail.com>
2021-08-14 00:04:03 +0700

    Update settings.py

--- dataset_management/settings.py
+++ dataset_management/settings.py
@@ -144,9 +144,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 import django_heroku
 django_heroku.settings(locals())
